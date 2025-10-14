bdbbf1ead68803c8be169d9b6389d5ad61c90240
CORP\aleksa.ristic <aleksa.ristic@devexpress.com>
2025-10-14 17:12:40 +0400

    Benchmarks

---
+++ src/Benchmarks/NullOrEmptyVSNullOrWhiteSpace.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Attributes;
+namespace Benchmarks;
+
+public class NullOrEmptyVSNullOrWhiteSpace {
+    static string str = "     ";
+    
+    [Benchmark]
+    public void IsNullOrWhiteSpace_100_000_Times() {
+        for(var i = 0; i < 100_000; i++)
+            string.IsNullOrWhiteSpace(str);
+    }
+
+    [Benchmark]
+    public void IsNullOrEmpty_100_000_Times() {
+        for(var i = 0; i < 100_000; i++)
+            string.IsNullOrEmpty(str);
+    }
+}
